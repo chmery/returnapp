fb66123483349d4000e035d899425b370a817cd8
chmery <karektv@gmail.com>
2022-08-06 19:06:03 +0200

    Create header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import classes from "./Header.module.css";
+
+const Header = () => {
+    return (
+        <header className={classes.header}>
+            <span>ReturnApp</span>
+        </header>
+    );
+};
+
+export default Header;
