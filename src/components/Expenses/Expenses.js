ddf7e3726743f6979bc388ecd3d42a83173aaac3
chmery <karektv@gmail.com>
2022-08-06 19:53:57 +0200

    Create button component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,17 @@
+import Button from "../UI/Button";
+
 import classes from "./Expenses.module.css";
+import PlusIcon from "./PlusIcon";
 
 const Expenses = () => {
     return (
-        <>
-            <div className={classes.expenses}>
-                <h1>Your Expenses</h1>
-                <p className={classes["start-text"]}>Start by adding your first expense.</p>
-            </div>
-        </>
+        <div className={classes.expenses}>
+            <h1>Your Expenses</h1>
+            <p className={classes["start-text"]}>Start by adding your first expense.</p>
+            <Button>
+                Add new expense <PlusIcon />
+            </Button>
+        </div>
     );
 };
 
