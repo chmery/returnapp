f7df53ec8a689dc910305b767d28f44502729572
chmery <karektv@gmail.com>
2022-08-13 14:03:16 +0200

    Create status in expense item

--- src/components/ExpensesList/ExpenseItem.js
+++ src/components/ExpensesList/ExpenseItem.js
@@ -4,12 +4,25 @@ import BrightCard from "../UI/Cards/BrightCard";
 import ManageButton from "../UI/Buttons/ManageButton";
 import RemoveButton from "../UI/Buttons/RemoveButton";
 
+const Status = (props) => {
+    const { amountReturned, amount } = props.expenseData;
+    const isExpenseFinished = amountReturned === amount;
+
+    const content = (
+        <>
+            {isExpenseFinished ? "Finished" : "Unfinished"}
+            <div className={isExpenseFinished ? classes.finished : classes.unfinished} />
+        </>
+    );
+
+    return <p className={classes.status}>{content}</p>;
+};
+
 const ExpenseItem = (props) => {
     const { expenseData } = props;
 
     const title = `${expenseData.title} `;
     const amount = `$${expenseData.amount}`;
-    const amountReturned = ` $${expenseData.amountReturned}`;
 
     const onRemoveHandler = () => props.onRemove(expenseData.id);
     const onManageHandler = () => props.onManage(expenseData.id);
@@ -17,15 +30,12 @@ const ExpenseItem = (props) => {
     return (
         <BrightCard>
             <div>
+                <Status expenseData={expenseData} />
                 <p className={classes.title}>
                     {title}
                     <span className={classes.amount}>
                         ( <span>{amount}</span> )
                     </span>
-                </p>
-                <p className={classes.returned}>
-                    Returned:
-                    <span>{amountReturned}</span>
                 </p>
             </div>
             <div className={classes.action}>
